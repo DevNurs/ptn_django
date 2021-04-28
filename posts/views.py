34af7b3cac6bf95f6fd8f12e279dafcc7661dd77
PythonNurs <nursultandev@gmail.com>
2021-04-28 17:16:27 +0600

    add create view and detail view

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 
 
@@ -9,3 +9,18 @@ def index(request):
 def get_data(request):
     posts = Post.objects.all()
     return render(request, 'posts/index.html', {'posts': posts})
+
+
+def post_data(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        file = request.FILES.get('file')
+        post_obj = Post.objects.create(title=title, description=description, image=file)
+        return redirect('data')
+    return render(request, 'posts/create.html')
+
+
+def detail_data(request, id):
+    posts = Post.objects.get(id=id)
+    return  render(request, 'posts/detail.html', {"posts": posts})
